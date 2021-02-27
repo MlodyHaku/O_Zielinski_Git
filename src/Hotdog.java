7a5d3c2b54dec4c1825aaa13201d9be1117c1b14
MlodyHaku <assassinczad@gmail.com>
2021-02-27 20:50:57 +0100

    Hotdog

--- src/Hotdog.java
+++ src/Hotdog.java
@@ -1,2 +1,9 @@
 public class Hotdog {
+    public String dodatki;
+    public Hotdog(String dodatki){
+        this.dodatki = dodatki;
+    }
+    public void showDodatki(){
+        System.out.println("Ten Hotdog ma takie dodatki: " + dodatki);
+    }
 }
